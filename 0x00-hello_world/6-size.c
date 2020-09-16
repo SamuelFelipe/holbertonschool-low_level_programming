0f404936ae6cbcb73d632d25ffc5fc2e35273cae
Samuel Correa <2354@holbertonschool.com>
2020-09-16 13:48:29 -0500

    first commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+
+	printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\nSize of a float: %d byte(s)\n", sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float));
+
+	return 0;
+}
