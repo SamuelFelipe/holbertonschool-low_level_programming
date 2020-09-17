25e993caa9d77ea9f14c57d557e16f3fab454a59
Samuel Correa <2354@holbertonschool.com>
2020-09-16 23:43:03 -0500

    fixed 6 file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
-int main(){
+/**
+ * main - prints the variables bytes
+ *
+ * Return: Always 0 (Succces)
+ *
+ **/
 
-	printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\nSize of a float: %d byte(s)\n", sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float));
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)
+		\nSize of a long int: %d byte(s)\nSize of a long long int: %d byte(s)
+		\nSize of a float: %d byte(s)\n", sizeof(char), sizeof(int),
+		sizeof(long int), sizeof(long long int), sizeof(float));
 
-	return 0;
+	return (0);
 }
