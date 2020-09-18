b4e9f89fc68645cf954079d1776952a1307a7989
Samuel Correa <2354@holbertonschool.com>
2020-09-17 19:49:15 -0500

    file 10-print_comb2.c upload

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations of single-digit numbers
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 100; i++)
+	{
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
+		if (i < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
