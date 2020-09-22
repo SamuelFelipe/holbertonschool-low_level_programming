e810cafdce76a9cae3c67ce98ec74cf926b3c697
Samuel Correa <2354@holbertonschool.com>
2020-09-22 13:53:10 -0500

    .h update

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <ctype.h>
 
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(char c);
+int _isalpha(char c);
