ef0ff8fbadff8c92a352fdcea95d030d3a67f44d
Samuel Correa <2354@holbertonschool.com>
2020-09-23 08:34:09 -0500

    10-add upload

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers
+ * @n: integer input
+ * @m: integer input
+ *
+ * Return: result in int
+ */
+
+int add(int n, int m)
+{
+	int r;
+
+	r = n + m;
+	return (r);
+}
