531445d03e9d96df39a05f2b5ed47fc7a69579fd
Samuel Correa <2354@holbertonschool.com>
2020-09-24 15:36:27 -0500

    Buzz fixed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,15 +14,15 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		else if (i % 3 == 0)
-			printf("Fizz");
+			printf("Fizz ");
 		else if (i % 5 == 0)
-			printf("Buzz");
+			printf("Buzz ");
 		else
-			printf("%i", i);
+			printf("%i ", i);
 	}
-	_putchar('\n');
+	putchar('\n');
 
 	return (0);
 }
