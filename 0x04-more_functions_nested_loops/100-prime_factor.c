ef857536fbd39308394dff2ef6f535243ae6114e
Samuel Correa <2354@holbertonschool.com>
2020-09-24 18:01:22 -0500

    100-prime_factor upload

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the largest prime factor of the number 612852475143
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+
+	int prime;
+	long int a;
+
+	a = 612852475143;
+
+	for (prime = 2; prime < a; prime++)
+		if (a % prime == 0)
+			a = a / prime; 
+
+	printf("%d\n", prime);
+
+	return (0);
+}
