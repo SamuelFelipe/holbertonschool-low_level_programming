229f142e69b886dabbb0bd2600127aa019b0fada
Samuel Correa <2354@holbertonschool.com>
2020-09-29 08:36:11 -0500

    4-print_rev.c without the do while bucle

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,12 +11,13 @@ void print_rev(char *s)
 {
 	int l = 0;
 
-	while (*(s + l) != '\0')
+	while (*(s + l + 1) != '\0')
 		l++;
 
-	do {
+	while (*(s + l) != '\0')
+	{
 		_putchar(*(s + l));
 		l--;
-	} while (*(s + l) != '\0');
+	}
 	_putchar('\n');
 }
