c0a8446fc52e81b78794d0162a288aed915c4f05
Samuel Correa <samuelfelipecorrea@gmail.com>
2020-10-01 14:19:32 -0500

    0-strcat.c upload

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: first part of the returned string
+ * @src: second part of the returned string
+ *
+ * Return: string concatenated
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int l = 1, i = 1, j;
+
+	while (*(dest + l) != '\0')
+		l++;
+	while (*(src + i) != '\0')
+		i++;
+
+	char *ret = (char *)malloc((l + i) * sizeof(char));
+
+	for (j = 0; j < l; j++)
+	{
+		if (*(dest + j) != '\0')
+			*(ret + j) = *(dest + j);
+		else
+			*(ret + j) = ' ';
+	}
+	for (j = 0; j < i; j++)
+		*(ret + (l + j)) = *(src + j);
+
+	return (ret);
+}
---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void _putchar(char);
+char *_strcat(char *, char *);
+
+
+#endif
