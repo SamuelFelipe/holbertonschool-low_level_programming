ee99b01136c15ba4e7d43f1ae4427e0a9c2d5cd8
Samuel Correa <samuelfelipecorrea@gmail.com>
2020-10-01 16:25:20 -0500

    2-strncpy.c upload and holberton.h actualiced

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - copy a string into dest
+ * @dest: target
+ * @src: source
+ * @n: number of bytes
+ *
+ * Return: result of the copy
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, l = 0, l1 = 0;
+
+	while (*(dest + l) != '\0')
+		l++;
+	while (*(src + l1) != '\0')
+		l1++;
+	for (i = 0; i < l && i < n; i++)
+	{
+		if (*(src + i) == '\0')
+		{
+			*(dest + i) = '\0';
+			break;
+		}
+		else
+			*(dest + i) = *(src + i);
+	}
+
+	return (dest);
+}
