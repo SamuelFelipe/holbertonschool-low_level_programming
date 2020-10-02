d46fe7115d0c423b26cc649507cdc13d381c812d
Samuel Correa <samuelfelipecorrea@gmail.com>
2020-10-01 21:59:56 -0500

    commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,12 +9,10 @@
 
 char *leet(char *str)
 {
-	int l = 0, i, j;
-	char *ch = {"aeotlAEOTL43071"};
+	int i, j;
+	char ch[] = "aeotlAEOTL43071";
 
-	while (*(str + l) != '\0')
-		l++;
-	for (i = 0; i < l; i++)
+	for (i = 0; *(str + i) != '\0'; i++)
 	for (j = 0; j < 10; j++)
 		if (*(str + i) == *(ch + j))
 			*(str + i) = *(ch + (j + 5));
