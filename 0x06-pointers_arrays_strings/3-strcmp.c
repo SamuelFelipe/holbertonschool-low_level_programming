0f0c1cbd228a96e912ff79276068d172c4a1eacb
Samuel Correa <samuelfelipecorrea@gmail.com>
2020-10-01 23:15:03 -0500

    qap

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,7 +16,7 @@ int _strcmp(char *s1, char *s2)
 	for (i = 0; *(s1 + i) != '\0' || *(s2 + i) != '\0'; i++)
 		if (*(s1 + i) != *(s2 + i))
 		{
-			n = ((*(s1 + i) - '0') - ((*s2 + i) - '0'));
+			n = *(s1 + i) - *(s2 + i);
 			break;
 		}
 	return (n);
