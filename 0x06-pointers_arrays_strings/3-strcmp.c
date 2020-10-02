951776ba563dcf6930ffac6edd5dc762e4ba27ff
Samuel Correa <samuelfelipecorrea@gmail.com>
2020-10-01 23:08:34 -0500

    3 is working
    deleted 2 bucles while

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,14 +11,9 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int n = 0, i, l = 0, l1 = 0;
+	int n = 0, i;
 
-	while (*(s1 + l) != '\0')
-		l++;
-	while (*(s2 + l1) != '\0')
-		l1++;
-
-	for (i = 0; i < l || i < l1; i++)
+	for (i = 0; *(s1 + i) != '\0' || *(s2 + i) != '\0'; i++)
 		if (*(s1 + i) != *(s2 + i))
 		{
 			n = ((*(s1 + i) - '0') - ((*s2 + i) - '0'));
