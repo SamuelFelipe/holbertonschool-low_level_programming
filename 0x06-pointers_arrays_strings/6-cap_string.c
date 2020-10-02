3f55d8cd324f866de0a101667ee8671d25180936
Samuel Correa <samuelfelipecorrea@gmail.com>
2020-10-01 20:50:24 -0500

    6 upload holberton.h update

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalize a string
+ * @str: string
+ *
+ * Return: string capitalized
+ */
+
+char *cap_string(char *str)
+{
+	int l = 0, i;
+
+	while (*(str + l) != '\0')
+		l++;
+
+	for (i = 0; i < l; i++)
+	{
+	switch (*(str + i))
+	{
+		case 9:
+		case 10:
+		case 32:
+		case 33:
+		case 34:
+		case 44:
+		case 46:
+		case 58:
+		case 59:
+		case 40:
+		case 41:
+		case 123:
+		case 125:
+			if (*(str + i + 1) >= 97 && *(str + i + 1) <= 122)
+				*(str + i + 1) -= 32;
+	}
+	}
+
+
+	return (str);
+}
