050fe213e1f6ee857911b0c94dcd6c16d7ec3587
Samuel Correa <samuelfelipecorrea@gmail.com>
2020-10-06 08:47:58 -0500

    9 upload

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * set_string - set the value of a pointer to a char
+ * @s: source
+ * @to: target
+ *
+ * Return: none
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
