a6a6bf7f6e7785d85ef8e6450b6b9a982d07413d
Samuel Correa <samuelfelipecorrea@gmail.com>
2020-10-11 10:59:28 -0500

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * main - print the file name
+ * 
+ * Return: Always 0 (Succes)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc)
+	printf("%s\n", argv[0]);
+	return (0);
+}
