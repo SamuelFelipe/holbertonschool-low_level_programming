d1144e641a6e4e00132c9344b8069250db6d8abd
Samuel Correa <samuelfelipecorrea@gmail.com>
2020-10-11 13:26:10 -0500

    hot fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,7 +10,15 @@
 
 int main(int argc, char *argv[])
 {
-	int i, n = -1, r = 0;
+	int i, j, n = -1, r = 0;
+
+	for (i = 1; i < argc; i++)
+	for (j = 0; argv[i][j] != '\0'; j++)
+		if (argv[i][j] < 48 || argv[i][j] > 57)
+		{
+			printf("Error\n");
+			return (1);
+		}
 
 	for (i = 1; i < argc; i++)
 	{
