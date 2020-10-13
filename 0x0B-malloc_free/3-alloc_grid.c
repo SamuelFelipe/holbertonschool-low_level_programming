103ce42e9cd427f655ca486d3dd2725f5804fdaf
Samuel Correa <samuelfelipecorrea@gmail.com>
2020-10-13 15:10:22 -0500

    3 commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	r = malloc(height * sizeof(int));
+	r = malloc(height * sizeof(int *));
 
 	if (r == NULL)
 		return (NULL);
