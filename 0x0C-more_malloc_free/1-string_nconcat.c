43ade44d8c6c49793d0f353e7307b7cc352ad0e4
Samuel Correa <samuelfelipecorrea@gmail.com>
2020-10-15 16:39:47 -0500

    1 upload

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * string_nconcat - concatenate the first string with the n first bytes of the second one
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes
+ *
+ * Return: on succes, pointer to the new array, othewise NULL
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j;
+	char *r;
+
+	if (strlen(s2) < n)
+		r = malloc(strlen(s1) + strlen(s2) + 1);
+	else if (n < strlen(s2))
+		r = malloc(strlen(s1) + n + 1);
+
+	if (r == NULL)
+		return (NULL);
+	for (i = 0; *(s1 + i); i++)
+		*(r +  i) = *(s1 + i);
+	for (j = 0; j < n && *(s2 + j); i++, j++)
+		*(r + i) = *(s2 + j);
+	*(r + i) = '\0';
+
+	return (r);
+}
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -4,8 +4,10 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 int _putchar(char);
-void *malloc_checked(unsigned int b);
+void *malloc_checked(unsigned int );
+char *string_nconcat(char *, char *, unsigned int);
 
 #endif
