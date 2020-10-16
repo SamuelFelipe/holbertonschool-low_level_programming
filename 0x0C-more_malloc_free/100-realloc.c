71691be0edcb5ad69cdb570fa40b44eab75f5027
Samuel Correa <samuelfelipecorrea@gmail.com>
2020-10-16 11:31:55 -0500

    100 upload

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * _realloc - reallocate memory
+ * @ptr - pointer to the memory previously allocated
+ * @old_size: <=
+ * @new_size: <=
+ *
+ * Return: on succes return pointer to the new allocated memory
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *r;
+
+	if (ptr == NULL)
+	{
+		r = malloc(new_size);
+		if (r == NULL)
+			return (NULL);
+		return (r);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	r = malloc(new_size);
+	if (r == NULL)
+		return (NULL);
+	free(ptr);
+	return (r);
+}
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -10,5 +10,6 @@ void *_calloc(unsigned int, unsigned int);
 void *malloc_checked(unsigned int );
 char *string_nconcat(char *, char *, unsigned int);
 int *array_range(int, int);
+void *_realloc(void *, unsigned int, unsigned int);
 
 #endif
