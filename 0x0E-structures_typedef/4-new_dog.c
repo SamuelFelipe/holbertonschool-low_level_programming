19fbc7dc72f49e82d76f1669c316274b5f00db4f
Samuel Correa <samuelfelipecorrea@gmail.com>
2020-10-19 13:32:51 -0500

    commit

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: <=
+ * @age: <=
+ * @owner: <=
+ *
+ * Return: on succes pointer, otherwise null
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog r;
+	struct dog *pr = &r;
+
+	(*pr).name = name;
+	(*pr).age = age;
+	(*pr).owner = owner;
+
+	return (pr);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,6 +22,7 @@ typedef struct dog
 
 void init_dog(struct dog *, char *, float, char *);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *, float, char *);
 
 
 #endif
