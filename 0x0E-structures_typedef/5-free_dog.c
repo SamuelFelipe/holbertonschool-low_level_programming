baabde8e49f7a92d058d87b6dc1b4fd3c8db3378
Samuel Correa <samuelfelipecorrea@gmail.com>
2020-10-19 14:17:39 -0500

    commit

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * free_dog - free dog
+ * @d: structure to free
+ *
+ * Return: none
+ */
+
+void free_dog(dog_t *d)
+{
+	free((*d).name);
+	free((*d).age);
+	free((*d).owner);
+	free(d);
+}
