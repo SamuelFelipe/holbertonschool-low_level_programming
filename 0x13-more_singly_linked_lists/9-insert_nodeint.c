15c644ed400778a0b98871d21275667ce5219fc6
Samuel Correa <samuelfelipecorrea@gmail.com>
2020-11-04 15:56:27 -0500

    commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	listint_t *new, *next, *beh;
+	listint_t *new, *beh;
 
 	if (*head == NULL)
 		return (NULL);
@@ -44,9 +44,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 
-	next = (*beh).next;
 	(*new).n = n;
-	(*new).next = next;
+	(*new).next = (*beh).next;
 	(*beh).next = new;
 	return (new);
 }
