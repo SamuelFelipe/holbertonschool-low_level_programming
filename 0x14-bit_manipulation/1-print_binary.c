89dcd9da340ef7595ddae773557ba46690626503
Samuel Correa <samuelfelipecorrea@gmail.com>
2020-11-06 03:43:33 -0500

    1 update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,26 +9,19 @@
 
 void print_binary(unsigned long int n)
 {
-	long int j, i, a;
-	unsigned long int aux;
+	unsigned long int r = 1024, m = 0;
 
 	if (n == 0)
 	{
-		_putchar(n + '0');
+		_putchar(48);
 		return;
 	}
-
-	for (aux = n, i = 0; aux != 1; aux /= 2, i++)
-		;
-	for (a = i, aux = n; a > 0; a--)
-	{
-		i = a;
-		for (; i > 0; i--, n /= 2)
-			j = (n / 2) % 2;
-		_putchar(j + '0');
-		n = aux;
-	}
-
-	j = n % 2;
-	_putchar(j + '0');
+	for (; r > 0; r >>= 1)
+		if (r & n)
+		{
+			_putchar(49);
+			m = 1;
+		}
+		else if (m == 1)
+			_putchar(48);
 }
