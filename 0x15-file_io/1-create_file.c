3ddea0ee287be69f858f7d0bcfecdca12949f093
SamuelFCorrea <70902050+SamuelFCorrea@users.noreply.github.com>
2020-11-09 16:47:17 -0500

    betty style

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 	while (*(text_content + l))
 		l++;
-	va = write(fd, text_content, l) ;
+	va = write(fd, text_content, l);
 	if (va != l)
 		return (-1);
 
