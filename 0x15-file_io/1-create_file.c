db0bc125a84cda9b2420fb7d19c3ddfbe0a92db8
Samuel Correa <samuelfelipecorrea@gmail.com>
2020-11-09 16:46:10 -0500

    1 upload

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * create_file - create and fill a file
+ * @filename: <=
+ * @text_content: <=
+ *
+ * Return: on succces 1, otherwise -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, va, l = 0;
+
+	if (filename == NULL)
+		return (1);
+
+	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+	while (*(text_content + l))
+		l++;
+	va = write(fd, text_content, l) ;
+	if (va != l)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -18,6 +18,8 @@ int _putchar(char);
 /* reads a text file and prints it to the POSIX standard output. */
 ssize_t read_textfile(const char *, size_t);
 
+/* create and fill a file. */
+int create_file(const char *filename, char *text_content);
 
 /*****************************************************************************/
 
