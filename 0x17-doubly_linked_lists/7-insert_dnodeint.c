34444747c962cce5da111308d1b9d6b192d68036
SamuelFCorrea <70902050+SamuelFCorrea@users.noreply.github.com>
2020-12-14 21:59:18 -0500

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,7 +24,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	for (i = 0; i < idx - 1 && prev; i++)
 		prev = prev->next;
-	if (i < idx)
+	if (i < idx -1)
 	{
 		free(new);
 		return (NULL);
