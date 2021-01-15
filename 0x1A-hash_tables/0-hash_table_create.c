1f5be450000237b4b11c496b140200a3f59b7ff8
Samuel Correa <samuelfelipecorrea@gmail.com>
2021-01-15 14:46:42 -0500

    First commit

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: hash table size
+ *
+ * Return: pointer to the newly ctreated hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new;
+
+	if (!size)
+		return(NULL);
+
+	new = malloc(sizeof(hash_table_t));
+	if (!new)
+		return(NULL);
+
+	new->size = size;
+	new->array = calloc(size,  sizeof(hash_node_t *));
+	if (!new->array)
+		return (NULL);
+	return (new);
+}
