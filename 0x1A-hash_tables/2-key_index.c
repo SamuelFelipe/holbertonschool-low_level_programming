3e26afbe52cc5482dfb7f9a9699a9df8cb8d386c
Samuel Correa <samuelfelipecorrea@gmail.com>
2021-01-15 20:37:03 -0500

    Upload

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - return the index of the node
+ * @key: key of the node
+ * @size: size of the array
+ *
+ * Return: node index
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return hash_djb2(key) % size;
+}
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,50 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: table to update
+ * @key: node key
+ * @value: node value
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new, *tmp;
+
+	if (!value || !ht || !key || !strlen(value))
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+		return (0);
+	new->next = NULL;
+	new->key = strdup(key);
+	if (!new->key)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = strdup(value);
+	if (!new->value)
+	{
+		free(new->key);
+		free(new);
+		return (0);
+	}
+	if (!ht->array[index])
+		ht->array[index] = new;
+	else
+	{
+		tmp = ht->array[index];
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new;
+	}
+
+	return (1);
+}
