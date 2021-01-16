2218aac1f9a2a883c73284dff0ceefd7fcb3bfed
SamuelFCorrea <70902050+SamuelFCorrea@users.noreply.github.com>
2021-01-16 15:31:23 -0500

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -16,7 +16,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	index = key_index((unsigned char *)key, ht->size);
 
-	if (!ht->array[index])
+	if (!ht->array[index] || !key)
 		return (NULL);
 
 	tmp = ht->array[index];
